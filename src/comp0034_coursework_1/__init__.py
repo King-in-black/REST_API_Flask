11d18cc9ebcbfaab03a8aa020592ccd62b3391fb
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-01-29 12:58:16 +0000

    Change the name of the router

--- src/comp0034_coursework_1/__init__.py
+++ src/comp0034_coursework_1/__init__.py
@@ -38,7 +38,7 @@ def create_app(test_config=None):
 
     # Models are defined in the models module, so you must import them before calling create_all, otherwise SQLAlchemy
     # will not know about them.
-    from Router.models import Trainer, Data, Player
+    from router.models import Trainer, Data, Player
     # Create the tables in the database
     # create_all does not update tables if they are already in the database.
     with app.app_context():
