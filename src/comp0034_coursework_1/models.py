bb6ddf97a1dfd8369f5debf3f231a177c63fccb8
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-01-29 11:41:53 +0000

    Construct the trainer class.

---
+++ src/comp0034_coursework_1/models.py
@@ -0,0 +1,58 @@
+
+# Adapted from https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/#define-models
+from typing import List
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from Rest import db
+class Trainer(db.Model):
+    __tablename__ ='trainer'
+    Trainer_ID: Mapped[str] = mapped_column(db.String(16), primary_key=True,nullable=False)
+    password: Mapped[str] = mapped_column(db.String(32), unique=False, nullable=False)
+    Player_ID: Mapped[str] = mapped_column(ForeignKey("player.Player_ID"))
+    player = relationship('Player',back_populates="trainer")
+    Data_ID: Mapped[str] = mapped_column(ForeignKey("data.Data_ID"))
+    data = relationship('Data',back_populates="trainer")
+# This uses the latest syntax for SQLAlchemy, older tutorials will show different syntax
+# SQLAlchemy provide an __init__ method for each model, so you do not need to declare this in your code
+class Player(db.Model):
+    __tablename__ = "player"
+    Player_ID: Mapped[str] = mapped_column(db.String(16), primary_key=True,unique=True,nullable=False)
+    region: Mapped[str] = mapped_column(db.Text, nullable=False)
+    notes: Mapped[str] = mapped_column(db.Text, nullable=True)
+    # one-to-many relationship with Event, the relationship in Event is called 'region'
+    # https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html#one-to-many
+    events: Mapped[List["Event"]] = relationship(back_populates="region")
+
+
+class Event(db.Model):
+    __tablename__ = "event"
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    type: Mapped[str] = mapped_column(db.Text, nullable=False)
+    year: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    country: Mapped[str] = mapped_column(db.Text, nullable=False)
+    host: Mapped[str] = mapped_column(db.Text, nullable=False)
+    NOC: Mapped[str] = mapped_column(ForeignKey("region.NOC"))
+    # add relationship to the parent table, Region, which has a relationship called 'events'
+    region: Mapped["Region"] = relationship("Region", back_populates="events")
+    start: Mapped[str] = mapped_column(db.Text, nullable=True)
+    end: Mapped[str] = mapped_column(db.Text, nullable=True)
+    duration: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    disabilities_included: Mapped[str] = mapped_column(db.Text, nullable=True)
+    countries: Mapped[str] = mapped_column(db.Text, nullable=True)
+    events: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    athletes: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    sports: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    participants_m: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    participants_f: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    participants: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    highlights: Mapped[str] = mapped_column(db.Text, nullable=True)
+
+    def __init__(self, email: str, password: str):
+        """
+        Create a new User object using hashing the plain text password.
+        :type password_string: str
+        :type email: str
+        :returns None
+        """
+        self.email = email
+        self.password = password
